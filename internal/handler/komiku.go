8d38fbeef0e9af0b9e72270dd962d217a4ccebd9
Arcie94 <arccodemix@gmail.com>
2026-01-20 08:49:02 +0700

    Fix: Use api.komiku.org with post_type parameter for search

--- internal/handler/komiku.go
+++ internal/handler/komiku.go
@@ -33,8 +33,8 @@ func (h *KomikuHandler) Search(c *fiber.Ctx) error {
 		return c.Status(400).JSON(fiber.Map{"error": "Query parameter 'q' is required"})
 	}
 
-	// Construct Search URL (using komiku.org like home endpoint)
-	searchURL := fmt.Sprintf("https://komiku.org/?s=%s", strings.ReplaceAll(query, " ", "+"))
+	// Construct Search URL (using api subdomain with post_type parameter like working exe)
+	searchURL := fmt.Sprintf("https://api.komiku.org/?post_type=manga&s=%s", strings.ReplaceAll(query, " ", "+"))
 
 	results, err := h.Service.FetchAndParseList(searchURL)
 	if err != nil {
